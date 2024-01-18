354f3e9e0fed30327a093b4c25260cdd8f7b61d4
Ptah <nilovanton6@gmail.com>
2024-01-18 23:45:23 +0300

    added some scripts and function for button

--- script/main.js
+++ script/main.js
@@ -1,1 +1,9 @@
-// lol
+const btn_click = document.getElementById("btn_click"),
+    title = document.querySelector('.name');
+
+btn_click.addEventListener('click',() => {
+    let x = Math.floor(Math.random()*(256));
+    let y = Math.floor(Math.random()*(256));
+    let z = Math.floor(Math.random()*(256));
+    title.style.color = `rgba(${x},${y},${z}, .7)`;
+})

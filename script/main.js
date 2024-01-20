82b5705b32f268f61fe1aa798b8716836776320b
Ptah <nilovanton6@gmail.com>
2024-01-20 22:54:51 +0300

    added leaderboard button and started creating of health scale

--- script/main.js
+++ script/main.js
@@ -1,9 +1,22 @@
-const btn_click = document.getElementById("btn_click"),
-    title = document.querySelector('.name');
+const healthScale = document.querySelector('.health-scale'),
+    artifactsBtn = document.querySelector('.artifacts-btn');
 
-btn_click.addEventListener('click',() => {
-    let x = Math.floor(Math.random()*(256));
-    let y = Math.floor(Math.random()*(256));
-    let z = Math.floor(Math.random()*(256));
-    title.style.color = `rgba(${x},${y},${z}, .7)`;
+
+let max_health = 1000,
+    health = 1000;
+
+addEventListener("click", function(){
+    if (health >= 0) {
+        health -= 100
+    }
+    healthScale.style.width = health / max_health * 100 + "%"
 })
+
+// healthScale.style.width = max_health / health + "%"
+
+// btn_click.addEventListener('click',() => {
+//     let x = Math.floor(Math.random()*(256));
+//     let y = Math.floor(Math.random()*(256));
+//     let z = Math.floor(Math.random()*(256));
+//     title.style.color = `rgba(${x},${y},${z}, .7)`;
+// })

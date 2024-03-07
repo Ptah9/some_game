5a4d8d47490ec589c44943df299863b089c472d6
Ptah_9 <nilovanton6@gmail.com>
2024-03-07 22:10:24 +0300

    Improved level generation: from 1 to 3 exits, more holes :D

--- script/generation.js
+++ script/generation.js
@@ -15,23 +15,28 @@ function getRandomArbitrary(min, max) {
 function Generation(entranceSide) {
     let rooms = 0;
     let level = [];
-    while(rooms < 10){
-        let entranceY = 0;
-        let entranceX = 0;
-        let nowX = 0;
-        let nowY = 0;
-        level = [
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-        ];
+    let exitQuantity = getRandomArbitrary(1, 4);
+    let entranceExist = false;
+    while(rooms < 15 || rooms > 40 || !entranceExist){
+
+            let entranceY = 0;
+            let entranceX = 0;
+            let nowX = 0;
+            let nowY = 0;
+            level = [
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+            ];
+            
+
         switch(entranceSide){
             case 1:     //left
                 entranceY = getRandomArbitrary(1,9);
@@ -78,51 +83,95 @@ function Generation(entranceSide) {
 
 
             break;
+                
+            }
+            for(let lol = 0; lol < exitQuantity; lol++) {
+            level[nowY][nowX] = 1;
+            let going = 1;
 
-            
-        }
-        level[nowY][nowX] = 1;
-        let going = 1;
+            while(going) {
+                let now = getRandomArbitrary(0,4)
+                switch(now) {
+                    case 0:
+                        nowX += 1;
+                        break;
+                    case 1:
+                        nowX -= 1;
+                        break;
+                    case 2:
+                        nowY += 1;
+                        break;
+                    case 3:
+                        nowY -= 1;
+                        break;
+                }
+                level[nowY][nowX] = 1;
+                if (nowX == 9 || nowY == 9 || nowX == 0 || nowY == 0){
+                    let exitX = nowX;
+                    let exitY = nowY;
+                    level[nowY][nowX] = 3;
+                    going = 0;
+                }
+            }
+            nowX = entranceX;
+            nowY = entranceY;
 
-        while(going) {
-            let now = getRandomArbitrary(0,4)
-            switch(now) {
-                case 0:
-                    nowX += 1;
-                    break;
-                case 1:
+
+            switch(entranceSide){
+                case 1:     //left
                     nowX -= 1;
                     break;
-                case 2:
-                    nowY += 1;
+    
+                case 2:     //right
+                    nowX += 1;
                     break;
-                case 3:
+    
+                case 3:     //top
                     nowY -= 1;
                     break;
-            }
-            level[nowY][nowX] = 1;
-            if (nowX == 9 || nowY == 9 || nowX == 0 || nowY == 0){
-                let exitX = nowX;
-                let exitY = nowY;
-                level[nowY][nowX] = 3;
-                going = 0;
-            }
+    
+                case 4:     //bottom
+                    nowY += 1;
+                    break;
+                    
+                }
+
         }
-
         rooms = 0;
-
+        entranceExist = false;
         for (let y = 0; y < 10; y++){
             for (let x = 0; x < 10; x++){
                 if (level[y][x] == 1){ 
                     rooms+=1
                 }
+                if (level[y][x] == 2){
+                    entranceExist = true;
+                }
             }
         }
-        
     }
-    return level
 
 
+    for (let y = 1; y < 9; y++){
+        for (let x = 1; x < 9; x++){
+            if (
+                
+                level[y][x] != 0 &&
+                level[y][x-1] != 0 &&
+                level[y-1][x-1] != 0 &&
+                level[y-1][x] != 0 &&
+                level[y-1][x+1] != 0 &&
+                level[y][x+1] != 0 &&
+                level[y+1][x+1] != 0 &&
+                level[y+1][x] != 0 &&
+                level[y+1][x-1] != 0){ 
+
+                level[y][x] = 0;
+                
+            }
+        }
+    }
+    return level    
 }
 
 

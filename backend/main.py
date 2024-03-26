8d09da71a0f837a1b50447b4baad01a7cfaa5a5e
Ptah_9 <nilovanton6@gmail.com>
2024-03-26 18:46:27 +0300

    backend/frontend

---
+++ backend/main.py
@@ -0,0 +1,12 @@
+from getpass import getpass
+from mysql.connector import connect, Error
+
+try:
+    with connect(
+        host="localhost",
+        user=input("Имя пользователя: "),
+        password=getpass("Пароль: "),
+    ) as connection:
+        print(connection)
+except Error as e:
+    print(e)
